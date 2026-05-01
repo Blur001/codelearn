888efec8477fbc3d1a3436681d27df61b5137117
Vince Cabinbin <vcc.blurberry@gmail.com>
2026-05-01 15:49:04 +0800

    3

---
+++ python/basics/learn3.py
@@ -0,0 +1,25 @@
+x_list = []
+x_list.append(1)
+x_list.append(2)
+x_list.append(3)
+print(x_list)
+
+# print(x_list[0])
+# print(x_list[1])
+# print(x_list[2])
+
+
+for x in x_list:
+    print(x)
+
+num_list = []
+string_list = []
+names_list = ["John", "Jane", "Doe"]
+
+num_list.append(1)
+num_list.append(2)
+string_list.append("Hello")
+string_list.append("World")
+print(num_list)
+print(string_list)
+print(f"Names: Second name in name list is {names_list[1]}")
