ca715164ff66319a57f4025806d8fc52e7be031e
Vince Cabinbin <vcc.blurberry@gmail.com>
2026-05-01 15:06:29 +0800

    2

--- python/basics/learn2.py
+++ python/basics/learn2.py
@@ -8,3 +8,16 @@ x_string = 'test1'
 x_string2 = "test2"
 print(x_string)
 print(x_string2)
+
+x = 1
+y = 2
+z = x + y
+print(z)
+
+x_string = "Hello"
+y_string = "World"
+z_string = x_string + " " + y_string
+print(z_string)
+
+x1, x2 = 1, 2
+print(x1, x2)
